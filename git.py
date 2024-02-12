197a0a8368494604b64ab1e79fd57ddb851ffef9
artur <arturzxcsf@gmail.com>
2024-02-12 19:29:07 +0200

    add file from for

--- git.py
+++ git.py
@@ -1,10 +1,6 @@
-def sum_numbers(numbers):
-    result = 0
-    for number in numbers:
-        if number > 0:
-            result += number
-    return result
+def sum_numbers(*numbers):
+    return sum(number for number in numbers if number > 0)
 
 
 if __name__ == "__main__":
-    print(sum_numbers([5, 10, -5, 15]))
+    print(sum_numbers(5, 10, -5, 15))
